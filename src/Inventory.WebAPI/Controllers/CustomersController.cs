6f0ebbbd2b3d9e6853b9e77138edf36cd4f73f69
TrinityTG <agharib@trinitytg.com>
2016-08-10 13:51:03 -0700

    Test 2

--- src/Inventory.WebAPI/Controllers/CustomersController.cs
+++ src/Inventory.WebAPI/Controllers/CustomersController.cs
@@ -16,7 +16,6 @@ namespace Inventory.WebAPI.Controllers
         public IEnumerable<string> Get()
         {
             return new string[] { "value1", "value2" };
-            // Test 1
         }
 
         // GET api/values/5
